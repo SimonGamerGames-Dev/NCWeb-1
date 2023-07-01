6e455776c43446318ae804e151670a6a69cc8dfa
NivocraftLino <linoschaefer10@gmail.com>
2023-07-01 21:53:10 +0200

    Website Wartung

---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="/css/main.css">
+    <title>Nivocraft.de | Wartung</title>
+</head>
+<body>
+    <div>
+        <span>
+            <strong>Nivocraft.de - Wartungsarbeiten</strong> <br>
+            Aufgrund von Schwierigkeiten im internen Ablauf müssen wir die Website vorübergehend deaktivieren. <br>
+            Wir bemühen uns, bald wieder online zu gehen. <br>
+            <strong>Vielen Dank für ihr Vertrauen!</strong>
+        </span>
+    </div>
+    <video id="background-video" autoplay loop muted>
+        <source src="/assets/images/video.mp4" type="video/mp4">
+    </video>
+</body>
+</html>
