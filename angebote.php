80313aa6cf6abbb1f2be5bd5a120bdcb025be093
NivocraftLino <linoschaefer10@gmail.com>
2023-07-03 20:51:07 +0200

    addf

--- angebote.php
+++ angebote.php
@@ -1,1 +1,22 @@
-<?php
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="/assets/css/main.css">
+    <link rel="stylesheet" href="/assets/css/extra_angebote.css">
+    <link rel="shortcut icon" href="/assets/images/nivocraft_2.png" type="image/x-icon">
+    <title>Nivocraft.de - Unsere Angebote</title>
+</head>
+<body>
+    <header>
+        <nav>
+            <a href="index.php">Start</a>
+            <a class="active"  href="angebote.php">Unser Angebot</a>
+            <a href="blog.php">Hilfestellungen</a>
+        </nav>
+        <h1>Willkommen auf der Startseite von Nivocraft.de!</h1>
+    </header>
+</body>
+</html>
